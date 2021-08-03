66a1648ac8afd0408950bdf8b3b9c1f58d724c2a
taha.noori <taha.noori@Student.Education.wa.edu.au>
2021-08-03 09:44:10 +0800

    First commit

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("Welcome to Cafe au lait")
 
 
+print("What would you like?")
+print("1. Cappacheeneo: $2.50")
+print("2. Latte: $1.70")
+print("3. Espresso: $ Something expensive")
+print(input("Place Order: "))
+
+
+
