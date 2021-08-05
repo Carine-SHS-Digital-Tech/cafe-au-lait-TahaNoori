cba1efb24459f93753da0387c04723b37dec8bd1
taha.noori <taha.noori@Student.Education.wa.edu.au>
2021-08-05 10:32:02 +0800

    First commit

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
+Cappuccino = "$2.50"
+Latte = "$3.50"
+Espresso = "$700"
+IcedCoffee = "$0"
+order = ('1. Cappuccino', '2. Latte', '3. Espresso', '4. Iced Coffee')
+
 print("Welcome to Cafe au lait")
-
-
 print("What would you like?")
-print("1. Cappacheeneo: $2.50")
-print("2. Latte: $1.70")
-print("3. Espresso: $ Something expensive")
-print(input("Place Order: "))
+
+print("1. Cappuccino")
+print("2. Latte")
+print("3. Espresso")
+print("4. Iced Coffee")
+
+print(input("PLace Order: "))
+while True:
+ if order == '1. Cappuccino':
+    print(Cappuccino)
+
+ if order == '2. Latte':
+    print(Latte)
+
+ if order == '3. Espresso':
+    print(Espresso)
+
+ if order == '4. Iced Coffee':
+    print(IcedCoffee)
+
+
+
 
 
 
